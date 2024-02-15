c111178daca3e45fa0b1fc572137660861b656ea
신승현 <133753824+sinseunghyeon@users.noreply.github.com>
2024-02-15 15:36:46 +0900

    [level 1] Title: 내적, Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.swift
+++ 프로그래머스/1/70128. 내적/내적.swift
@@ -1,13 +1,21 @@
 import Foundation
 
 func solution(_ a:[Int], _ b:[Int]) -> Int {
+    
     guard (a.count >= 1 && a.count <= 1000) && (b.count >= 1 && b.count <= 1000) else {
-        return 0;
+        return -2000;
     }
+    
     var result: Int = 0;
     
     for i in 0..<a.count {
+        
+        guard (a[i] >= -1000 && a[i] <= 1000) && (b[i] >= -1000 && b[i] <= 1000) else {
+            return -2000;
+        }
+        
         result += a[i] * b[i];
     }
+    
     return result;
 }
