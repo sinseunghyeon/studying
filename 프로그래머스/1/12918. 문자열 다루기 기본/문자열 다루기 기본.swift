e92417ef606955245a746945f1404b53ce8b25ab
신승현 <133753824+sinseunghyeon@users.noreply.github.com>
2024-02-19 15:38:29 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -0,0 +1,13 @@
+func solution(_ s:String) -> Bool {
+    
+    guard s.count >= 1 && s.count <= 8 else {
+        return false;
+    }
+    
+    if s.count == 4 || s.count == 6 {
+        return s.allSatisfy({ $0.isNumber });
+        
+    } else {
+        return false;
+    }
+}
