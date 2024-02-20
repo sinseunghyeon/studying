8f6adca6b85dd63c00727c4c653c9ebe4e66e311
신승현 <133753824+sinseunghyeon@users.noreply.github.com>
2024-02-20 15:14:46 +0900

    [level 1] Title: 행렬의 덧셈, Time: 13.12 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.swift
@@ -8,7 +8,7 @@ func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
     
     for i in 0..<arr1.count {
         for j in 0..<arr1[i].count {
-            result[i][j] = arr1[i][j] + arr2[i][j];
+            result[i][j] += arr2[i][j];
         }
     }
     return result;
