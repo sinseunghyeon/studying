7f903c99c150f931080f58e26593f7c1f2b42297
신승현 <133753824+sinseunghyeon@users.noreply.github.com>
2024-02-23 16:38:32 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.swift
+++ 프로그래머스/1/131705. 삼총사/삼총사.swift
@@ -4,13 +4,12 @@ func solution(_ number:[Int]) -> Int {
     var result: Int = 0;
     
     for i in 0..<number.count {
-        for j in 0..<number.count {
-            for k in 0..<number.count {
-                if (i != j && j != k && k != i) {
-                    result += (number[i] + number[j] + number[k] == 0) ? 1 : 0;
-                }
+        for j in i + 1..<number.count {
+            for k in j + 1..<number.count {
+                result += (number[i] + number[j] + number[k] == 0) ? 1 : 0;
             }
         }
     }
-    return result / 6;
+    
+    return result;
 }
