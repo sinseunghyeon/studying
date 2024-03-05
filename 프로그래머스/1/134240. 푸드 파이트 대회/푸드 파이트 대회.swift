bcdf349de71947822edfa49ff3ea7725730d3e2a
신승현 <133753824+sinseunghyeon@users.noreply.github.com>
2024-03-05 09:55:58 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
@@ -3,13 +3,9 @@ import Foundation
 func solution(_ food:[Int]) -> String {
     var result: String = "";
     
-    for i in 0..<food.count {
-        if i == 0 {
-            continue;
-        } else {
-            for j in 0..<food[i] / 2 {
+    for i in 1..<food.count {
+        for j in 0..<food[i] / 2 {
                 result += String(i);
-            }
         }
     }
     result += "0" + result.reversed();
