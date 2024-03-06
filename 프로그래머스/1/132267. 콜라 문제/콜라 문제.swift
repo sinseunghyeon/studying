289f2559ca633bbfcac42d312ae289589a62b454
신승현 <133753824+sinseunghyeon@users.noreply.github.com>
2024-03-06 09:16:09 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int, _ n:Int) -> Int {
-    var result: Int = 0;
-    var bottles: Int = n;
+    var result: Int = 0; // 새로 받은 콜라
+    var bottles: Int = n; // 현재 가지고 있는 빈 콜라
     
     while a <= bottles {
         result += ((bottles / a) * b);
