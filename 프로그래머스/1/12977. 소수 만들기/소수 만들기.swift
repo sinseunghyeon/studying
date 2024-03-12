34896425a9e8436ea2a391e48054a2411daae78b
신승현 <133753824+sinseunghyeon@users.noreply.github.com>
2024-03-12 09:46:56 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.swift
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.swift
@@ -1,14 +1,15 @@
 import Foundation
 
 func solution(_ nums:[Int]) -> Int {
-    var result: Int = 0;
+    
+    var result: Int = 0; // 소수 개수 결과 값 변수
     
     for i in 0..<nums.count {
         for j in i..<nums.count {
             for k in j..<nums.count {
-                if nums[i] != nums[j] && nums[j] != nums[k] {
-                    var sum = nums[i] + nums[j] + nums[k];
-                    result += decimal(sum);
+                if nums[i] != nums[j] && nums[j] != nums[k] { // 배열 내의 3개의 숫자가 서로 같지 않을 때의 조건
+                    var sum = nums[i] + nums[j] + nums[k]; // 3개의 숫자의 합
+                    result += prime(sum); // 소수를 구하는 함수
                 }
             }
         }
@@ -17,7 +18,8 @@ func solution(_ nums:[Int]) -> Int {
     return result;
 }
 
-func decimal(_ number: Int) -> Int{
+// 소수를 구하는 함수
+func prime(_ number: Int) -> Int{
     var count: Int = 0;
     
     for i in 1...number {
@@ -26,5 +28,5 @@ func decimal(_ number: Int) -> Int{
         }
     }
     
-    return count == 2 ? 1 : 0;
+    return count == 2 ? 1 : 0; // 소수면 1을 반환 아니면 0을 반환
 }
