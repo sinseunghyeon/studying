1cb74bb8a12780ede4e12f43a399c2f61636b62b
신승현 <133753824+sinseunghyeon@users.noreply.github.com>
2024-03-19 16:19:26 +0900

    [level 1] Title: 문자열 나누기, Time: 18.94 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.swift
@@ -1,28 +1,28 @@
 import Foundation
 
 func solution(_ s:String) -> Int {
-    var result: Int = 0;
-    var sQueue: [Character] = Array(s);
-    var firstChar: Character = sQueue[0];
-    var sameCount: Int = 0;
-    var differentCount: Int = 0;
-    var isTrue: Bool = false;
+    var result: Int = 0; // 결과값 카운트
+    var sQueue: [Character] = Array(s); // 문자열 배열로 저장
+    var firstChar: Character = sQueue[0]; // 문자열 첫번째 문자
+    var sameCount: Int = 0; // 값이 같을 때의 카운트
+    var differentCount: Int = 0; // 값이 다를 때의 카운트
+    var isTrue: Bool = false; // 
     
      while !sQueue.isEmpty {
         let pop = sQueue.removeFirst();
         
-        if isTrue {
+        if isTrue { // 첫번째 문자를 pop으로 설정
             firstChar = pop;
             isTrue = false;
         }
         
         if firstChar == pop {
-            sameCount += 1;
+            sameCount += 1; // 같을 때
         } else {
-            differentCount += 1;
+            differentCount += 1; // 다를 때
         }
         
-        if sameCount == differentCount || sQueue.isEmpty {
+        if sameCount == differentCount || sQueue.isEmpty { // 카운트 값이 같을때 또는 배열이 비어있을 때
             result += 1;
             isTrue = true;
             sameCount = 0;
