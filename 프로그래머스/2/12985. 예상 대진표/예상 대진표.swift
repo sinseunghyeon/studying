f6eba390477bde6403df4e91a809dbf2b6d422d6
신승현 <133753824+sinseunghyeon@users.noreply.github.com>
2024-04-03 09:46:47 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
@@ -5,9 +5,9 @@ func solution(_ n:Int, _ a:Int, _ b:Int) -> Int {
     var a = a
     var b = b
     
-    while a != b {
-        a = a % 2 == 0 ? a / 2 : (a + 1) / 2
-        b = b % 2 == 0 ? b / 2 : (b + 1) / 2
+    while a != b { // a == b 이면 같은 라운드 이므로 종료
+        a = a % 2 == 0 ? a / 2 : (a + 1) / 2 // a가 짝수이면 /2 홀수이면 +1 후 /2
+        b = b % 2 == 0 ? b / 2 : (b + 1) / 2 // b가 짝수이면 /2 홀수이면 +1 후 /2
         result += 1
     }
 
